77fc6b2b91afd6b90b6272c38897716853f93b88
starmelt <tlemrats@googlemail.com>
2011-07-27 15:09:07 +0200

    yay! SpriteText works

--- bettertext/text.js
+++ bettertext/text.js
@@ -9,10 +9,34 @@ window.onload = (function() {
      */
     Crafty.c("SpriteText", {
         _registeredSpriteFonts: {},
-        _defaultMapping: " !\"#$%&#'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_´" +
+        _defaultMapping: " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_´" +
                             "abcdefghijklmnopqrstuvwxyz{|}~", // ASCII Characters 32-126
+        _entities: [],
+        _oldText: "",
                             
-        init: function() {  
+        init: function() {
+            this.bind("EnterFrame", function(obj) {
+                var txt = this._text.split(""), // String to Array
+                    tileSize = this._registeredSpriteFonts[this._font],
+                    l, pos, type, e, ch;
+                if (tileSize && this._oldText !== this._text) {
+                    this._oldText = this._text;
+                    for (i in this._entities) {
+                        this._entities[i].destroy();
+                    }
+                    for (i in txt) {
+                        l = txt[i];
+                        pos = {x: this.x + i * tileSize, y: this.y + tileSize};
+                        type = obj.type === "DOM" ? "DOM" : "Canvas";
+                        ch = Crafty.e(this.charName(this._font, l))._image ? l : l.toUpperCase();
+                        console.log(ch + ", " + pos.x);
+                        e = Crafty.e("2D, " + type + ", " + this.charName(this._font, ch))
+                                .attr({x: pos.x, y: pos.y, w: tileSize, h: tileSize});
+                        //console.log(e);
+                        this._entities.push(e);
+                    }
+                }
+            });
         }, 
         /**@
          * Sets the Text.
@@ -41,12 +65,13 @@ window.onload = (function() {
                 spriteMap = {},
                 currentChar,
                 x, y;
-            for (x = 0; x <= w; x++) {
+            for (x = 0; x < w; x++) {
                 for (y = 0; y < h; y++) {
                     currentChar = mapping.charAt(x + y * w);
                     spriteMap[this.charName(fontName, currentChar)] = [x, y];
                 }
             }
+            console.log(spriteMap);
             Crafty.sprite(tileSize, url, spriteMap);
             this._registeredSpriteFonts[fontName] = tileSize;
             return this.font(fontName);
@@ -57,8 +82,13 @@ window.onload = (function() {
         
     });
     
-    var text = Crafty.e("2D, Canvas, Image, SpriteText")
+    var text = Crafty.e("2D, Canvas, Image, Mouse, SpriteText")
                 .attr({x: 160, y: 96, w: 32, h: 32})
                 .registerFont("BlueBubble", 16, "../img/BlueBubbleFont.png")
                 .text("Hello World!");
+                
+    text.bind("Click", function (e) {
+        console.log("!!");
+        text.text("clicked!");
+    });
 });
